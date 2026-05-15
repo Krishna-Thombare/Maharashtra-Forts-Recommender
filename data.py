3c533cda807c93a4e70e75c6abe25f07ba848fde
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-05-15 12:43:26 +0530

    remove data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data_cleaned.csv')
-
-districts  = sorted(df['district'].unique().tolist())
-fort_types = sorted(df['type'].unique().tolist())
-difficulties = ['Easy', 'Medium', 'Hard']
-seasons    = sorted(df['best_season'].unique().tolist())
-conditions = sorted(df['current_condition'].unique().tolist())
